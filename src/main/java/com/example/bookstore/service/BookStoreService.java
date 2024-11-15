ca2ae7fb1736950fc109c47a30caae3ac4d8a726
Swati Thakare <thakareswati999@gmail.com>
2024-11-15 16:15:50 +0100

    Implemented discount logic for multiple distinct books.

--- src/main/java/com/example/bookstore/service/BookStoreService.java
+++ src/main/java/com/example/bookstore/service/BookStoreService.java
@@ -3,13 +3,29 @@ package com.example.bookstore.service;
 import com.example.bookstore.model.BookBasket;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+
 @Service
 public class BookStoreService {
 
     private static final double BOOK_PRICE = 50.0;
 
     public double calculatePrice(BookBasket basket){
-        return basket.getBooks().size() * BOOK_PRICE;
+        int distinctBooks = new HashSet<>(basket.getBooks()).size();
+        int totalBooks = basket.getBooks().size();
+
+        double discount = getDiscount(distinctBooks);
+        return distinctBooks * BOOK_PRICE * (1 - discount) + (totalBooks - distinctBooks) * BOOK_PRICE;
+    }
+
+    private double getDiscount(int distinctBooks){
+        return switch (distinctBooks) {
+            case 2 -> 0.05;
+            case 3 -> 0.10;
+            case 4 -> 0.20;
+            case 5 -> 0.25;
+            default -> 0.0;
+        };
     }
 
 }
