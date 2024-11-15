77cf51c87ce0e2622d2f48280ce486b5444bf086
Swati Thakare <thakareswati999@gmail.com>
2024-11-15 16:29:42 +0100

    Added tests for discounts with 3,4 and 5 different books.

--- src/test/java/com/example/bookstore/service/BookStoreServiceTest.java
+++ src/test/java/com/example/bookstore/service/BookStoreServiceTest.java
@@ -1,6 +1,5 @@
 package com.example.bookstore.service;
 
-
 import com.example.bookstore.model.BookBasket;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,7 +25,6 @@ public class BookStoreServiceTest {
 
     @Test
     void shouldApplyFivePercentDiscountForTwoDifferentBooks(){
-
         BookBasket basket = new BookBasket();
         basket.addBook("Clean Code");
         basket.addBook("The Clean Coder");
@@ -36,4 +34,33 @@ public class BookStoreServiceTest {
         assertEquals(95.0, price, "Price for two different books be 95 EUR");
     }
 
+    @Test
+    void shouldApplyTenPercentDiscountForThreeDifferentBooks(){
+        BookBasket basket = new BookBasket();
+        basket.addBook("Clean Code");
+        basket.addBook("The Clean Coder");
+        basket.addBook("Clean Architecture");
+        assertEquals(135.0, bookStoreService.calculatePrice(basket));
+    }
+
+    @Test
+    void shouldApplyTwentyPercentDiscountForFourDifferentBooks(){
+        BookBasket basket = new BookBasket();
+        basket.addBook("Clean Code");
+        basket.addBook("The Clean Coder");
+        basket.addBook("Clean Architecture");
+        basket.addBook("TDD by Example");
+        assertEquals(160.0, bookStoreService.calculatePrice(basket));
+    }
+
+    @Test
+    void shouldApplyTwentyFivePercentDiscountForFiveDifferentBooks(){
+        BookBasket basket = new BookBasket();
+        basket.addBook("Clean Code");
+        basket.addBook("The Clean Coder");
+        basket.addBook("Clean Architecture");
+        basket.addBook("TDD by Example");
+        basket.addBook("Working Effectively with Legacy Code");
+        assertEquals(187.5, bookStoreService.calculatePrice(basket));
+    }
 }
